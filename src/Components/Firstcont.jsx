6e9b7bd77c42a989eae2c944fa2d2117108658e8
Deepak <deepakkumarkakarana@gmail.com>
2024-03-29 23:06:39 +0530

    inquiry form changes

--- src/Components/Firstcont.jsx
+++ src/Components/Firstcont.jsx
@@ -2,7 +2,9 @@ import './First.css';
 import {useEffect} from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 export default function Firstcont() {
 
@@ -10,31 +12,52 @@ export default function Firstcont() {
   const [email, setemail] = useState("");
   const [errors, setErrors] = useState({});
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const errors = {};
-    if (!name.trim()) {
-      alert(errors.name = 'Name is required');
-    }
-    if (!email.trim()) {
+  
+  const form = useRef();
 
-      alert(errors.email = 'Email is required');
-
-    } 
-    else if 
-    (!/\S+@\S+\.\S+/.test(email)) 
-    {
-      errors.email = 'Email is invalid';
-    }
-    if (Object.keys(errors).length === 0) {
+  const sendEmail = (e) => {
+        event.preventDefault();
+        const errors = {};
+        if (!name.trim()) {
+          alert(errors.name = 'Name is required');
+        }
+        if (!email.trim()) {
     
-      console.log('Form submitted:', { name, email });
-    } else {
-     
-      setErrors(errors);
-    }
-    console.log({ name, email});
-  };
+          alert(errors.email = 'Email is required');
+    
+        } 
+        else if 
+        (!/\S+@\S+\.\S+/.test(email)) 
+        {
+          errors.email = 'Email is invalid';
+        }
+        if (Object.keys(errors).length === 0) {
+        
+          console.log('Form submitted:', { name, email });
+        } else {
+         
+          setErrors(errors);
+        }
+        console.log({ name, email});
+
+         e.preventDefault();
+
+        emailjs
+          .sendForm('service_7nga6vq', 'template_yc4e13e', form.current, {
+            publicKey: 'cFochbQ771rQN3gwe',
+          })
+          .then(
+            () => {
+              alert('Thankyou for connect with me.Now you have sucessfully connected.');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+            );
+            form.current.reset();
+    
+    };
+
 
 
   useEffect(()=>{
@@ -46,14 +69,14 @@ export default function Firstcont() {
 
       <div className="firstcont">
         
-      <form data-aos="fade-right" onSubmit={handleSubmit} className="Card">
+      <form ref={form} data-aos="fade-right" onSubmit={sendEmail} className="Card">
       <h2 id='head'>Enter  your details to get started!</h2>
       <h3>Enter Your Name Here</h3>
-      <input type="text"  onChange={(event) => setName(event.target.value)} value={name} placeholder='Enter Your Name Here' />
+      <input name='user_name' type="text"  onChange={(event) => setName(event.target.value)} value={name} placeholder='Enter Your Name Here' />
       <h3>Enter your Email</h3>
-      <input type="email" value={email} onChange={(event)=> setemail(event.target.value)} placeholder='Enter your email here' /><br /><br /><br />
+      <input name='user_email' type="email" value={email} onChange={(event)=> setemail(event.target.value)} placeholder='Enter your email here' /><br /><br /><br />
       {errors.email && <span style={{ color: 'red' }}>{errors.email}</span>}
-      <button>For Enquarie</button>
+      <button type='submit' value="Send" >For Enquarie</button>
       </form>
       </div>
       
